0484e6e6683fd27579f70bc92aff21249564363c
delin230825 <delinyotia044@gmail.com>
2025-03-04 21:39:42 +0800

    Pertemuan 1

---
+++ Pertemuan01/src/pertemuan01/Pertemuan01.java
@@ -0,0 +1,16 @@
+package pertemuan01;
+
+/**
+ *
+ * @author delin yotia
+ */
+public class Pertemuan01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("Belajar Bahasa Java");
+    }
+    
+}
